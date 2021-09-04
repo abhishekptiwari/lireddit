64a82b90fe2fa4c093df178659e3e005159b6679
Abhishek Tiwari <abhishektiwariaman@gmail.com>
2021-09-04 17:04:26 +0530

    CRUD operation fixed

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,22 +1,50 @@
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolvers{
+  //Read--All Data
   @Query(() => [Post])
   posts(@Ctx() {em}:MyContext):Promise<Post[]>{
 
     return em.find(Post,{})
   }
-
+ 
+  //Read--Data(individual)
   @Query(() =>Post ,{nullable:true})
-  post(
-    @Arg("id",()=>Int) id:number,
-    @Ctx() { em }: MyContext 
-    ):Promise<Post | null>{
-
-    return em.findOne(Post, { id })
+  post(@Arg("id") id:number,@Ctx() { em }: MyContext):Promise<Post | null>{
+    return em.findOne(Post, { id });
   }
 
+  //Create--Data
+  @Mutation(() => Post)
+  async createPost(@Arg("title") title:string,@Ctx() { em }: MyContext):Promise<Post>{
+    const post=em.create(Post,{title});
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  //Update--Data
+  @Mutation(() => Post,{nullable:true})
+  async updatePost(@Arg("id") id:number,@Arg("title",()=>String,{nullable:true}) title:string,
+  @Ctx() { em }: MyContext 
+  ):Promise<Post | null >{
+     const post=await em.findOne(Post,{id})
+     if(!post){
+       return null;
+     }
+     if(typeof title !=="undefined"){
+       post.title=title;
+       await em.persistAndFlush(post);
+     }
+    return post;
+  }
+
+  //Delete--Data
+  @Mutation(() => Boolean)
+  async deletePost(@Arg("id") id:number,@Ctx() { em }: MyContext):Promise<boolean >{
+    await em.nativeDelete(Post,{id})
+    return true;
+  }
 }
