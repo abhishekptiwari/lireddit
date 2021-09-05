0ebe022fffd7fe1888d1b0d6f90a9163f8e63229
Abhishek Tiwari <abhishektiwariaman@gmail.com>
2021-09-05 17:43:05 +0530

    Field validated without token

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -31,8 +31,29 @@ class UserResponse {
 
 @Resolver()
 export class UserResolvers {
-  @Mutation(()=>User)
-  async register(@Arg('options') options:UsernamePasswordInput,@Ctx(){em}:MyContext){
+  @Mutation(()=>UserResponse)
+  async register(@Arg('options') options:UsernamePasswordInput,@Ctx(){em}:MyContext):Promise<UserResponse>{
+
+    if(options.username.length <= 2){
+      return {
+        errors:[
+          {
+          field:'username',
+          message:'Length must be greater than 2'
+          },
+      ]
+      }
+    }
+    if(options.username.length <= 5){
+      return {
+        errors:[
+          {
+          field:'Password',
+          message:'Password must be greater than 5 chracter'
+          },
+      ]
+      }
+    }
       const hashedPassword = await argon2.hash(options.password);
       const user = em.create(User,{
         username:options.username,
@@ -41,7 +62,9 @@ export class UserResolvers {
       
        await em.persistAndFlush(user);
       
-      return user;
+      return {
+         user
+      };
   }
 
 
